52d8148f25106205b5446110e5c8342ec54655dd
silvi9653 <92724708+silvi9653@users.noreply.github.com>
2021-11-08 10:55:24 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -65,3 +65,50 @@ plot(L2011$B2_sre, col=clg)
 
 #dev.off() to clean the workspace on R
 
+# day 3.....
+
+plot(L2011$B1_sre)
+#assign the color in the blue band
+clb<- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(L2011$B1_sre, col=clb)    
+
+#chain two images in multiframe graph
+par(mfrow=c(1,2)) #the first number is the namer of the row in the multiframe the second number of column
+plot(L2011$B1_sre, col=clb)
+clg<- colorRampPalette(c("dark green","green","light green"))(100)
+plot(L2011$B2_sre, col=clg)
+
+# Exercise: invert the number of rows and the colomn
+par(mfrow=c(2,1))
+plot(L2011$B1_sre, col=clb)
+plot(L2011$B2_sre, col=clg)
+
+# Exercise: plot the first four band with two rows and two colums
+par(mfrow=c(2,2))
+plot(L2011$B1_sre, col=clb)
+plot(L2011$B2_sre, col=clg)
+clr<- colorRampPalette(c("dark red","red","pink"))(100) #light red not exist we usde pink instead
+plot(L2011$B3_sre, col=clr)
+clNIR<- colorRampPalette(c("red","orange","yellow"))(100) #color for NIR
+plot(L2011$B4_sre, col=clNIR)
+
+dev.off()
+#RGB setting
+plotRGB(L2011,r=3, g=2, b=1, stretch="Lin")#linear stretch, the number in the function is for the 3 band  
+
+#use NIR band, witch the vegetation reflect a lot
+plotRGB(L2011,r=4, g=3, b=2, stretch="Lin") # we have exclude the blue band and transfert the other band
+
+#change the channel to plot the NIR band
+plotRGB(L2011,r=3, g=4, b=2, stretch="Lin") # we can see change of humidity inside the forest
+plotRGB(L2011,r=3, g=2, b=4, stretch="Lin") # yellow part cutted forest
+
+# multiframe
+par(mfrow=c(2,2))
+plotRGB(L2011,r=3, g=2, b=1, stretch="Lin")
+plotRGB(L2011,r=4, g=3, b=2, stretch="Lin")
+plotRGB(L2011,r=3, g=4, b=2, stretch="Lin") 
+plotRGB(L2011,r=3, g=2, b=4, stretch="Lin")
+
+
+
