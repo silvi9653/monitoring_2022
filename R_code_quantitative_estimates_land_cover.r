166fea019530ba2a0ca9c12f83e802029ea35949
silvi9653 <92724708+silvi9653@users.noreply.github.com>
2021-12-06 10:55:01 +0100

    Update R_code_quantitative_estimates_land_cover.r

--- R_code_quantitative_estimates_land_cover.r
+++ R_code_quantitative_estimates_land_cover.r
@@ -1,16 +1,17 @@
 #R_code_quantitative_estimates_land_cover
 #install new packages
-#install.packages("RStoolbox")
+#install.packages("RStoolbox") 
 #install.packages("ggplot2")
 #install.packages("gridExtra")
-library(RStoolbox)
-library(ggplot2)
+library(RStoolbox) #we use this packeges for classification
+library(ggplot2) #we use this packeges for the graph 
+library(gridExtra)# we use this packeges for sum of graph
 #call library raster and set the working directory
 library(raster)
 setwd("C:/lab/")
 #use lapplay for import due images
 # first create a list the files available
-rlist<-list.files(pattern="defor")
+rlist<-list.files(pattern="defor") #pattern the common name of the element we want to import
 rlist #to see the component of the object
 #second import the images togheter with lapplay
 list_rast<-lapply(rlist,brick)
@@ -19,16 +20,16 @@ list_rast #to see the information of the images
 plot(list_rast[[1]])
 #plot RGB first images
 #defor: NIR 1, red 2, green 3
-plotRGB(list_rast[[1]], r=1, g=2, b=3) #NIR is teh first band we put it in red componement
+plotRGB(list_rast[[1]], r=1, g=2, b=3) #NIR is the first band we put it in red componement
 #assign a simple name 
 l1992<-list_rast[[1]]
-plotRGB(l1992, r=1, g=2, b=3)
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 #do the same thing with the second picture
 l2006<-list_rast[[2]]
-plotRGB(l2006, r=1, g=2, b=3)
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
 #library(RStollbox)
-#unsupervised classification
+#unsupervised classification: to classified the pixel to the type of ground
 l1992c<-unsuperClass(l1992, nClasses=2)
 l1992c
 plot(l1992c$map)#plot the images
@@ -47,7 +48,8 @@ propforest<-305999/341292
 # forest: 0.89659 = 90%
 #build data frame
 cover<-c("Forest","Agriculture")
-prop1992<-c(0.89659, 0.10341)
+#prop1992<-c(0.89659, 0.10341) insted of using number below we use the objects
+prop1992<-c(propforest, propagri)
 #create the data frame
 proportion1992<-data.frame(cover,prop1992)
 #to see the table
@@ -57,3 +59,36 @@ proportion1992
 #create ggplot(percentages, aesthetics (x=*name of first column* , y=*name of second column*, color=))
 #different kind of ggplot, we use geom_bar (bar charts)
 ggplot(proportion1992, aes(x=cover, y= prop1992, color= cover)) + geom_bar(stat="identity",fill="white")
+
+#day2........
+#repete the all code for the second picture
+l2006c<-unsuperClass(l2006, nClasses=2)
+l2006c
+plot(l2006c$map)#plot the images
+#value 1 = agricoultural areas and water
+#value2 = forest
+#use function freq function
+freq(l2006c$map)
+# number of pixel of 1 178671
+# number of pixel of 2 164055
+# total of pixel 342726 
+#propotion
+propagri_2006<-164055/342726 
+propforest_2006<-178671/342726 
+# agricolture wather :  0.4786768 = 48%
+# forest: 0.5213232 = 52%
+#build data frame
+cover<-c("Forest","Agriculture")
+prop1992<-c(propforest, propagri) #recall for simple view
+prop2006<-c(propforest_2006, propagri_2006)
+#create the data frame
+proportion<-data.frame(cover,prop1992, prop2006)
+#to see the table
+proportion
+
+#create ggplot 
+#different kind of ggplot, we use geom_bar (bar charts)
+p1<-ggplot(proportion, aes(x=cover, y= prop1992, color= cover)) + geom_bar(stat="identity",fill="white") + ylim(0,1) # graph 1992
+p2<-ggplot(proportion, aes(x=cover, y= prop2006, color= cover)) + geom_bar(stat="identity",fill="white") + ylim(0,1) #graph 2006
+#library(gridExtra)
+grid.arrange(p1,p2,nrow=1)
