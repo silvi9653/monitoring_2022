584ab11735b3202d8c1a59b56510f64d59164240
silvi9653 <92724708+silvi9653@users.noreply.github.com>
2021-12-22 11:46:50 +0100

    Create R_code_biomes.r

---
+++ R_code_biomes.r
@@ -0,0 +1,22 @@
+# install.package("vegan")
+#install.packages("sdm")
+library(vegan)
+library(sdm)
+# Set the working directory
+setwd("C:/lab/")  
+load("biomes_multivar.RData") #load the file
+ls() #see the store element in r
+#we interest biomes and biomes_types 
+biomes #to see the structure of the element
+biomes_types #to see the type of biomesa
+
+multivar<-decorana(biomes)
+multivar
+
+plot(multivar)
+
+#grouping of species
+attach(biomes_types)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind="ehull", lwd=3)
+
+ordispider(multivar, type, col=c("black","red","green","blue"), label=T)
