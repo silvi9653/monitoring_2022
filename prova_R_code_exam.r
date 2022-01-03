49cbe3fba4a4542a2c96a8f07db38130cb340b1f
silvi9653 <92724708+silvi9653@users.noreply.github.com>
2022-01-03 11:12:30 +0100

    Update prova_R_code_exam.r

--- prova_R_code_exam.r
+++ prova_R_code_exam.r
@@ -1,7 +1,7 @@
 #This is the code for the final exam
 #download images from copernicus program
 #Soil Water Index - 10-daily SWI 12.5km Global V3
-#I choose the first 10 days for each month in the 2021
+#I want to analyze who the vegetation condition the soilwater content and the temperature  
 
 #import all the library 
 #install.packages("ncdf4")
@@ -24,7 +24,7 @@ setwd("C:/lab/exam")
 rlist<- list.files(pattern="SWI") #create a list with a common pattern
 rlist 
 
-list_rast<-lapply(rlist,brick) #plot the images
+list_rast<-lapply(rlist,raster) #plot the images
 list_rast #view the information of the pictures
 
 #make the stack, to have all of them togheter
@@ -40,7 +40,7 @@ plot(wsoil, col=cl)
 rlist_temp<- list.files(pattern="LST") #create a list with a common pattern
 rlist_temp
 
-list_temp<-lapply(rlist_temp,brick) #plot the images
+list_temp<-lapply(rlist_temp,raster) #plot the images
 list_temp #view the information of the pictures
 
 #make the stack, to have all of them togheter
@@ -50,15 +50,35 @@ temp
 #plot the images 
 plot(temp, col=cl)
 
-rlist_veg<- list.files(pattern="FCOVER") #create a list with a common pattern
-rlist_veg
+rlist_veg1<- list.files(pattern="LAI") #create a list with a common pattern
+rlist_veg1
 
-list_veg<-lapply(rlist_veg,brick) #plot the images
-list_veg #view the information of the pictures
+list_veg1<-lapply(rlist_veg1,raster) #plot the images
+list_veg1 #view the information of the pictures
 
 #make the stack, to have all of them togheter
-veg<-stack(list_veg)
-veg
+veg1<-stack(list_veg1)
+veg1
+
+rlist_veg2<- list.files(pattern="FCOVER") #create a list with a common pattern
+rlist_veg2
+
+list_veg2<-lapply(rlist_veg2,raster) #plot the images
+list_veg2 #view the information of the pictures
+
+#make the stack, to have all of them togheter
+veg2<-stack(list_veg2)
+veg2
+
+rlist_veg3<- list.files(pattern="NDVI") #create a list with a common pattern
+rlist_veg3
+
+list_veg3<-lapply(rlist_veg3,raster) #plot the images
+list_veg3 #view the information of the pictures
+
+#make the stack, to have all of them togheter
+veg3<-stack(list_veg3)
+veg3
 
 #plot the images 
 plot(veg, col=cl)
@@ -68,10 +88,19 @@ plot(wsoil, col=cl)
 plot(temp, col=cl)
 plot(veg, col=cl)
 
-ext<-c(-50,0,-100,0) #create the extenction of coordiantes first
+ext<-c(-50,20,-100,0) #create the extenction of coordiantes first
 #crop use the stack and than extarct the images
-crop_2011<-crop(veg$X2011.02.03,ext) 
+crop_2011<-crop(wsoil$X2011.01.18,ext) 
 crop_2020<-crop(veg$X2020.02.03,ext) 
 par(mfrow=c(2,1))
 plot(crop_2011, col=cl)
 plot(crop_2020, col=cl)
+
+ggplot()+geom_raster(wsoil, mapping=aes(x=x, y=y, fill=X2011.01.18))+
+scale_fill_viridis()#asign the defoult viridis palette
+
+gp1<-ggplot()+geom_raster(veg, mapping=aes(x=x, y=y, fill=X2019.01.24 ))+
+scale_fill_viridis()#asign the defoult viridis palette
+gp2<-ggplot()+geom_raster(veg, mapping=aes(x=x, y=y, fill=X2020.01.24 ))+
+scale_fill_viridis()#asign the defoult viridis palette
+gp1/gp2
