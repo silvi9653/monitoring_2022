1887fab367fe6864712421fa8a56211957aa8202
silvi9653 <92724708+silvi9653@users.noreply.github.com>
2022-01-14 20:41:53 +0100

    Update R_code_greenland.r

--- R_code_greenland.r
+++ R_code_greenland.r
@@ -6,22 +6,22 @@
 #install.packages("ggplot2")
 #install.packages("patchwork")
 #install.packages("viridis")
-library(raster)
-library(RStoolbox)
-library(ggplot2)
-library(patchwork)
-library(viridis)
+library(raster) #for work with raster file
+library(RStoolbox) #useful for remote sensing image processing
+library(ggplot2) # for create graphics
+library(patchwork) #for multiframe graphics
+library(viridis)# for plot the color palette
 
 #set the working directory
 setwd("C:/lab/greenland")
 
-#upload all the pictures with lapply
+#upload all the images with lapply
 rlist<-list.files(pattern="lst")
 rlist #see the elements of the list
 
-#upload the pictures
-list_rast<-lapply(rlist, raster) #raster because the pictures have single layer
-list_rast #see the information of the pictures
+#upload the images
+list_rast<-lapply(rlist, raster) #raster because the images have single layer
+list_rast #see the information of the images
 
 #make the stack of the file
 grl<-stack(list_rast)
@@ -29,7 +29,7 @@ grl<-stack(list_rast)
 #plot the images
 #first create a color ramp palette
 cl<-colorRampPalette(c("blue","light blue","pink","yellow"))(100)
-plot(grl, col=cl)
+plot(grl, col=cl)#assign the color palette
 
 #use ggplot with the package ggplot2 and RStoolbox
 #create a ggplot of the first and final images 2000 vs. 2015 
@@ -40,7 +40,7 @@ scale_fill_viridis(option="magma")+ggtitle("LST in 2000")
 gp2<-ggplot()+geom_raster(grl$lst_2015, mapping=aes(x=x, y=y, fill=lst_2015))+
 scale_fill_viridis(option="magma")+ggtitle("LST in 2015")
 
-#use the packages patchwork for create a multiframe with the two pictures
+#use the packages patchwork for create a multiframe with the two images
 # gp1/gp2 one in top of the other
 gp1+gp2 
 
@@ -53,14 +53,14 @@ par(mfrow=c(1,2)) # (nrow,ncol)
 hist(grl$lst_2000)
 hist(grl$lst_2015)
 
-#now do this for all the pictures
+#now do this for all the graphics
 par(mfrow=c(2,2)) 
 hist(grl$lst_2000)
 hist(grl$lst_2005)
 hist(grl$lst_2010)
 hist(grl$lst_2015)
 
-dev.off()
+dev.off() #clear the divice
 #see the distribution of value 
 #first plot the two images 
 plot(grl$lst_2010, grl$lst_2015)
@@ -88,7 +88,7 @@ plot(grl$lst_2005, grl$lst_2000, xlim=c(12500,15000), ylim=c(12500,15000))
 #finish
 
 #simple and faster way to do
-#use th stack with the function pairs=create a scatterplot matrices
+#use th stack with the function pairs=create a scatterplot matrix
 pairs(grl)
 
 
