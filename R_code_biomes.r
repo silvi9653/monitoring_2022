4446f42ec2590ab807295c9b1f1c82cdfc710c6b
silvi9653 <92724708+silvi9653@users.noreply.github.com>
2022-01-14 20:48:57 +0100

    Update R_code_biomes.r

--- R_code_biomes.r
+++ R_code_biomes.r
@@ -1,7 +1,7 @@
 #install.packages("vegan")
 #install.packages("sdm")
-library(vegan)
-library(sdm)
+library(vegan) #for diversity analysis and other functions
+library(sdm)#for developing species distribution models 
 # Set the working directory
 setwd("C:/lab/")  
 load("biomes_multivar.RData") #load the file
@@ -10,19 +10,19 @@ ls() #see the store element in r
 biomes #to see the structure of the element
 biomes_types #to see the type of biomes
 
-#plot per species matrix
-multivar<-decorana(biomes)
+#import per species matrix
+multivar<-decorana(biomes) #decorana:Detrended Correspondence Analysis
 multivar
 
-plot(multivar)
+plot(multivar) #plot the data
 
 #grouping of species
-attach(biomes_types)
-ordiellipse(multivar, type, col=c("black","red","green","blue"), kind="ehull", lwd=3)
-ordispider(multivar, type, col=c("black","red","green","blue"), label=T)
+attach(biomes_types)#used to access the variables present in the data framework without calling the data frame
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind="ehull", lwd=3) #ordiellipse draws lines or polygon s for ellipses by groups
+ordispider(multivar, type, col=c("black","red","green","blue"), label=T) #draws a 'spider' diagram where each point is connected to the group centroid with segments 
 
-pdf("multivar.pdf")
+pdf("multivar.pdf") #create the pdf
 plot(multivar)
-ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3) 
 ordispider(multivar, type, col=c("black","red","green","blue"), label = T) 
-dev.off()
+dev.off()#close the pdf
